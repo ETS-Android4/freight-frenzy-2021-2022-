0d70af9bd03c3da7d7634886bca62170951249dd
Nick Free <nalibre@gamil.com>
2021-12-12 20:39:18 -0600

    Working Auto that just goes to warehouse

--- Team6168/src/main/java/org/firstinspires/ftc/team6168/AutoBlueWarehouse.java
+++ Team6168/src/main/java/org/firstinspires/ftc/team6168/AutoBlueWarehouse.java
@@ -13,9 +13,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous(name="myAuto", group="chad")
+@Autonomous(name="AutoBlueWarehouse", group="chad")
 public class AutoBlueWarehouse extends LinearOpMode {
-    //
+
     DcMotor frontleft;
     DcMotor frontright;
     DcMotor backleft;
@@ -28,55 +28,53 @@ public class AutoBlueWarehouse extends LinearOpMode {
     Double cpi = (cpr * gearratio)/(Math.PI * diameter); //counts per inch, 28cpr * gear ratio / (2 * pi * diameter (in inches, in the center))
     Double bias = 0.8;//default 0.8
     Double meccyBias = 0.9;//change to adjust only strafing movement
-    //
+
     Double conversion = cpi * bias;
     Boolean exit = false;
-    //
+
     BNO055IMU imu;
     Orientation angles;
     Acceleration gravity;
-    //
+
     public void runOpMode(){
-        //
+
         initGyro();
-        //
-        frontleft = hardwareMap.dcMotor.get("Frontleft");
-        frontright = hardwareMap.dcMotor.get("Frontright");
-        backleft = hardwareMap.dcMotor.get("Backleft");
-        backright = hardwareMap.dcMotor.get("Backright");
+
+        backleft = hardwareMap.get(DcMotor.class, "Backleft");
+        frontleft = hardwareMap.get(DcMotor.class, "Frontleft");
+        backright = hardwareMap.get(DcMotor.class, "Backright");
+        frontright = hardwareMap.get(DcMotor.class, "Frontright");
 
         frontright.setDirection(DcMotorSimple.Direction.REVERSE);
         backright.setDirection(DcMotorSimple.Direction.REVERSE);
-        //
+
         waitForStartify();
-        //
-        moveToPosition(28.4, 0.2);
-        //
+
+        moveToPosition(20, 0.3);
+
+
     }
-    //
-    /*
-    This function's purpose is simply to drive forward or backward.
-    To drive backward, simply make the inches input negative.
-     */
+
+
     public void moveToPosition(double inches, double speed){
-        //
+
         int move = (int)(Math.round(inches*conversion));
-        //
+
         backleft.setTargetPosition(backleft.getCurrentPosition() + move);
         frontleft.setTargetPosition(frontleft.getCurrentPosition() + move);
         backright.setTargetPosition(backright.getCurrentPosition() + move);
         frontright.setTargetPosition(frontright.getCurrentPosition() + move);
-        //
+
         frontleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         frontright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //
+
         frontleft.setPower(speed);
         backleft.setPower(speed);
         frontright.setPower(speed);
         backright.setPower(speed);
-        //
+
         while (frontleft.isBusy() && frontright.isBusy() && backleft.isBusy() && backright.isBusy()){
             if (exit){
                 frontright.setPower(0);
@@ -92,7 +90,7 @@ public class AutoBlueWarehouse extends LinearOpMode {
         backleft.setPower(0);
         return;
     }
-    //
+
     /*
     This function uses the Expansion Hub IMU Integrated Gyro to turn a precise number of degrees (+/- 5).
     Degrees should always be positive, make speedDirection negative to turn left.
@@ -104,14 +102,14 @@ public class AutoBlueWarehouse extends LinearOpMode {
         telemetry.addData("Speed Direction", speedDirection);
         telemetry.addData("Yaw", yaw);
         telemetry.update();
-        //
+
         telemetry.addData("stuff", speedDirection);
         telemetry.update();
-        //
+
         double first;
         double second;
-        //</editor-fold>
-        //
+
+
         if (speedDirection > 0){//set target positions
             //<editor-fold desc="turn right">
             if (degrees > 10){
@@ -131,16 +129,16 @@ public class AutoBlueWarehouse extends LinearOpMode {
                 first = devertify(yaw);
                 second = devertify(-degrees + devertify(yaw));
             }
-            //
+
             //</editor-fold>
         }
-        //
+
         //<editor-fold desc="Go to position">
         Double firsta = convertify(first - 5);//175
         Double firstb = convertify(first + 5);//-175
-        //
+
         turnWithEncoder(speedDirection);
-        //
+
         if (Math.abs(firsta - firstb) < 11) {
             while (!(firsta < yaw && yaw < firstb) && opModeIsActive()) {//within range?
                 angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
@@ -152,7 +150,7 @@ public class AutoBlueWarehouse extends LinearOpMode {
                 telemetry.update();
             }
         }else{
-            //
+
             while (!((firsta < yaw && yaw < 180) || (-180 < yaw && yaw < firstb)) && opModeIsActive()) {//within range?
                 angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
                 gravity = imu.getGravity();
@@ -163,12 +161,12 @@ public class AutoBlueWarehouse extends LinearOpMode {
                 telemetry.update();
             }
         }
-        //
+
         Double seconda = convertify(second - 5);//175
         Double secondb = convertify(second + 5);//-175
-        //
+
         turnWithEncoder(speedDirection / 3);
-        //
+
         if (Math.abs(seconda - secondb) < 11) {
             while (!(seconda < yaw && yaw < secondb) && opModeIsActive()) {//within range?
                 angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
@@ -194,7 +192,7 @@ public class AutoBlueWarehouse extends LinearOpMode {
             backright.setPower(0);
         }
         //</editor-fold>
-        //
+
         frontleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -204,30 +202,30 @@ public class AutoBlueWarehouse extends LinearOpMode {
         backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
-    //
+
     /*
     This function uses the encoders to strafe left or right.
     Negative input for inches results in left strafing.
      */
     public void strafeToPosition(double inches, double speed){
-        //
+
         int move = (int)(Math.round(inches * cpi * meccyBias));
-        //
+
         backleft.setTargetPosition(backleft.getCurrentPosition() - move);
         frontleft.setTargetPosition(frontleft.getCurrentPosition() + move);
         backright.setTargetPosition(backright.getCurrentPosition() + move);
         frontright.setTargetPosition(frontright.getCurrentPosition() - move);
-        //
+
         frontleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         frontright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //
+
         frontleft.setPower(speed);
         backleft.setPower(speed);
         frontright.setPower(speed);
         backright.setPower(speed);
-        //
+
         while (frontleft.isBusy() && frontright.isBusy() && backleft.isBusy() && backright.isBusy()){}
         frontright.setPower(0);
         frontleft.setPower(0);
@@ -235,7 +233,7 @@ public class AutoBlueWarehouse extends LinearOpMode {
         backleft.setPower(0);
         return;
     }
-    //
+
     /*
     A tradition within the Thunder Pengwins code, we always start programs with waitForStartify,
     our way of adding personality to our programs.
@@ -243,7 +241,7 @@ public class AutoBlueWarehouse extends LinearOpMode {
     public void waitForStartify(){
         waitForStart();
     }
-    //
+
     /*
     These functions are used in the turnWithGyro function to ensure inputs
     are interpreted properly.
@@ -264,7 +262,7 @@ public class AutoBlueWarehouse extends LinearOpMode {
         }
         return degrees;
     }
-    //
+
     /*
     This function is called at the beginning of the program to activate
     the IMU Integrated Gyro.
@@ -277,11 +275,11 @@ public class AutoBlueWarehouse extends LinearOpMode {
         parameters.loggingEnabled      = true;
         parameters.loggingTag          = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-        //
+
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
     }
-    //
+
     /*
     This function is used in the turnWithGyro function to set the
     encoder mode and turn.
@@ -291,11 +289,11 @@ public class AutoBlueWarehouse extends LinearOpMode {
         backleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //
+
         frontleft.setPower(input);
         backleft.setPower(input);
         frontright.setPower(-input);
         backright.setPower(-input);
     }
-    //
+
 }
